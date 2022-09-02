eeff024fbdee4efa06b586b57eacebeb1df558cb
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-09-02 15:50:34 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,6 +1,6 @@
 #
 #
-# SSH CEK
+# CEK OPENSSH
 ssh_service=$(/etc/init.d/ssh status | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
 # STATUS SERVICE  SSH 
 if [[ $ssh_service == "running" ]]; then 
@@ -9,7 +9,7 @@ else
    status_ssh="${RED}  Not Running ${NC}  ( Error )"
 fi
 #
-# SSH DROPBEAR
+# CEK SSH DROPBEAR
 dropbear_status=$(/etc/init.d/dropbear status | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
 # STATUS SERVICE DROPBEAR
 if [[ $dropbear_status == "running" ]]; then 
@@ -17,6 +17,13 @@ if [[ $dropbear_status == "running" ]]; then
 else
    status_beruangjatuh="${RED}  Not Running ${NC}  ( Error )${NC}"
 fi
+
+# CEK SLOWDNS CLIENT
+status_slowdns_client=$(systemctl status client-sldns | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+
+# CEK SLOWDNS SERVER
+status_slowdns_server=$(systemctl status server-sldns | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+
 
 # STUNNEL5
 stunnel_service=$(/etc/init.d/stunnel5 status | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
@@ -48,7 +55,8 @@ echo -e "\E[44;1;39m            ⇱ Service Information ⇲             \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "❇️ SSH OpenSSH             :$status_ssh"
 echo -e "❇️ SSH Dropbear            :$status_beruangjatuh""
-echo -e "❇️ SSH SlowDNS             :$status_slowdns"
+echo -e "❇️ SlowDNS Server          :$status_slowdns_server"
+echo -e "❇️ SlowDNS Client          :$status_slowdns_client"
 echo -e "❇️ SSLH                    :$sosslh"
 echo -e "❇️ SSH Stunnel5            :$status_stunnel"
 echo -e "❇️ SSH Websocket TLS       :$swstls"
