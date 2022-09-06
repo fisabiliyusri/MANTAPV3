3c48798dea38afc068943098420b97acc4f26dc9
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-09-06 19:22:50 +0700

    Update and rename auto-pointing.sh to auto-dns.sh

--- cloudflare/auto-dns.sh
+++ cloudflare/auto-dns.sh
@@ -4,25 +4,23 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
-rm -f /root/domain
-rm -f /etc/v2ray/domain
-rm -f /etc/xray/domain
-rm -rf /etc/xray/domain
-rm -rf /root/nsdomain
-rm -rf /var/lib/crot/ipvps.conf
-rm nsdomain
-rm domain
 mkdir -p /usr/bin/xray
 mkdir -p /usr/bin/v2ray
 mkdir -p /etc/xray
 mkdir -p /etc/v2ray
-echo "$SUB_DOMAIN" >> /etc/v2ray/domain
-#
+mkdir -p /var/lib/crot
+rm nsdomain
+rm domain
+rm -f /root/domain
+rm -rf /root/nsdomain
+rm -rf /etc/xray/domain
+rm -rf /var/lib/crot/ipvps.conf
+# Buat Dns Records Secara Otomatis
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 DOMAIN=mantapxsl.my.id
-SUB_DOMAIN=onichan-${sub}.mantapxsl.my.id
-NS_DOMAIN=zerosl-${sub}.mantapxsl.my.id
+SUB_DOMAIN=zerochan-${sub}.mantapxsl.my.id
+NS_DOMAIN=zerokun-${sub}.mantapxsl.my.id
 CF_ID=slinfinity69@gmail.com
 CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
 set -euo pipefail
@@ -75,12 +73,11 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"NS","name":"'${NS_DOMAIN}'","content":"'${SUB_DOMAIN}'","ttl":120,"proxied":false}')
-rm -rf /etc/xray/domain
-rm -rf /root/nsdomain
+
 echo "IP=""$SUB_DOMAIN" >> /var/lib/crot/ipvps.conf
-echo "Host : $SUB_DOMAIN"
 echo $SUB_DOMAIN > /root/domain
-echo "Host SlowDNS : $NS_DOMAIN"
-echo "$NS_DOMAIN" >> /root/nsdomain
 echo "$SUB_DOMAIN" >> /etc/xray/domain
+echo "$NS_DOMAIN" >> /root/nsdomain
+echo "Host : $SUB_DOMAIN"
+echo "Host SlowDNS : $NS_DOMAIN"
 cd
