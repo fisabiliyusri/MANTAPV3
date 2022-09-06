def6732616da4508bd0c6e794801413e91b7fd82
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-09-06 19:43:11 +0700

    Update delete-akun.sh

--- menu/menu-xray/delete-akun.sh
+++ menu/menu-xray/delete-akun.sh
@@ -3,8 +3,6 @@
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
 
-
-
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
@@ -38,7 +36,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
     systemctl restart xray > /dev/null 2>&1
     clear
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo " V2RAY Account Deleted Successfully"
+    echo " XRAY Account Deleted Successfully"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo " Client Name : $user"
     echo " Expired On  : $exp"
